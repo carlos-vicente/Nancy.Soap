cf5c940b0d0dad491e435a3c2cd80c56fe44126e
Carlos Vicente <carlosvicente200@hotmail.com>
2015-03-23 22:21:30 +0000

    Added scripts to execute fixie unit tests on solidbuild

---
+++ tests/RunTests.ps1
@@ -0,0 +1,13 @@
+# Script to execute all fixie tests on projects in this directory
+
+$testAssemblies = Get-ChildItem *.Tests/bin/**/*.Tests.dll
+
+$fixieConsole = Get-ChildItem "../packages/Fixie.*/lib/**/Fixie.Console.exe"
+
+Foreach($test in $testAssemblies){
+	
+	$cmd = $fixieConsole.FullName
+	$arg = $test.FullName
+	
+	& $cmd $arg
+}
